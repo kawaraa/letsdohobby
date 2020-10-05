c26a747661b75df3959eb92cbd5d256576d06e31
Ahmad Kawara <kawaraa4@gmail.com>
2020-10-05 15:27:56 +0200

    Fixes

--- app/frontend/ssr/index.js
+++ app/frontend/ssr/index.js
@@ -1,12 +1,12 @@
-const templateEngine = require("./utility/template-engine");
 const CustomError = require("../../server/src/domain/model/custom-error");
 const Validator = require("../../server/src/my-npm/validator");
+const indexHtml = require("./view/index.html");
+const errorMessage = require("./view/layout/error.html");
+const navbar = require("./view/layout/navbar.html");
+const postList = require("./view/post-list.html");
+const postById = require("./view/post-by-id.html");
 
 module.exports = (router, firewall, MysqlDatabaseProvider, config) => {
-  const navbar = templateEngine.compile("layout/navbar.html");
-  const indexHtml = templateEngine.compile("index.html");
-  const errorMessage = templateEngine.compile("layout/error.html");
-
   // Repositories
 
   // Handlers
@@ -19,9 +19,7 @@ module.exports = (router, firewall, MysqlDatabaseProvider, config) => {
       const query = `SELECT t1.id, t1.activity, t1.description, t1.participants, t1.mediaUrls, t1.createdAt, t1.startAt, t2.displayName, (SELECT COUNT(member) FROM feeds.chat WHERE postId=t1.id) AS members FROM feeds.post t1 JOIN user.profile t2 ON t1.owner=t2.owner ORDER BY t1.createdAt DESC LIMIT ? OFFSET ?`;
       const posts = await MysqlDatabaseProvider.query(query, [20, 0]);
 
-      const postList = templateEngine.render("post-list.html", posts);
-
-      response.send(indexHtml(postList, navbar(false)));
+      response.send(indexHtml(postList(posts), navbar(false)));
     } catch (error) {
       response.status(500).end(indexHtml(errorMessage(CustomError.validate(error).message), navbar(false)));
     }
@@ -37,9 +35,7 @@ module.exports = (router, firewall, MysqlDatabaseProvider, config) => {
       result[0].members = await MysqlDatabaseProvider.query(query, id);
       result[0].mediaUrls = Validator.stringToArray(result[0].mediaUrls);
 
-      const post = templateEngine.render("post-by-id.html", result[0]);
-
-      response.send(indexHtml(post, navbar(false)));
+      response.send(indexHtml(postById(result[0]), navbar(false)));
     } catch (error) {
       response.status(500).end(indexHtml(errorMessage(CustomError.validate(error).message), navbar(false)));
     }
